692d82c541e1720062819af65213e9a9e310afc4
Lejo1 <Lejo_1@web.de>
2019-07-31 13:58:58 +0200

    Percentage!

--- init.lua
+++ init.lua
@@ -7,6 +7,14 @@ https://github.com/Lejo1/minetest/tree/version_string4
 Just access the data using /client_matcher list
 ]]
 
+--[[Infos about clients:
+MT Official: 5.0.0-githash-Android or 0.4.16/17/17.1 or special -dirty
+
+Forks(sorted by most players):
+NAME123 Spieler: 27 ||| 26 ||| 0 ||| 4 ||| 13 ||| 0.4.13
+
+]]
+
 local storage = minetest.get_mod_storage()
 
 minetest.register_on_joinplayer(function(player)
@@ -19,12 +27,38 @@ minetest.register_on_joinplayer(function(player)
           local data = info.protocol_version.."@"..info.serialization_version.."@"..
           info.major.."@"..info.minor.."@"..info.patch.."@"..info.version_string
           storage:set_int(data, storage:get_int(data) + 1)
-        else minetest.log("error", "Client Matcher: You need a server build with access to this see README.md for help!")
+        else minetest.log("error", "Client Matcher: You need a server build with access to advanced player information see README.md for help!")
         end
       end
     end
   end)
 end)
+
+local function get_string_by_info(info)
+  local str = ""
+  local total = 0
+  local array = storage:to_table().fields
+  for _, numb in pairs(array) do
+    total = total + numb
+  end
+  if not info then
+    str = str.."[join count/percentage] protocol_version, ser_vers, major, minor, patch, version_string"
+    for index, count in pairs(array) do
+      local data = string.split(index, "@")
+      str = str.."\n["..count.."/"..100*(count/total).."%] "..table.concat(data, " ||| ")
+    end
+  elseif info.protocol_version and info.serialization_version and info.major
+  and info.minor and info.patch and info.version_string then
+    local data = info.protocol_version.."@"..info.serialization_version.."@"..
+    info.major.."@"..info.minor.."@"..info.patch.."@"..info.version_string
+    local count = storage:get_int(data)
+    local data_table = string.split(data, "@")
+    str = str.."[join count/percentage] protocol_version, ser_vers, major, minor, patch, version_string"
+    str = str.."\n["..count.."/"..100*(count/total).."%] "..table.concat(data_table, " ||| ")
+  else str = "Got wrong info."
+  end
+  return str
+end
 
 minetest.register_chatcommand("client_matcher", {
   description = "Shows which kind of client join your server!",
@@ -33,21 +67,14 @@ minetest.register_chatcommand("client_matcher", {
   func = function(name, params)
     local split = string.split(params, " ")
     if params == "list" then
-      minetest.chat_send_player(name, "[join count] protocol_version, ser_vers, major, minor, patch, version_string")
-      for index, count in pairs(storage:to_table().fields) do
-        local data = string.split(index, "@")
-        minetest.chat_send_player(name, "["..count.."] "..table.concat(data, " ||| "))
-      end
+      minetest.chat_send_player(name, get_string_by_info())
     elseif params == "reset" then
       storage:from_table(nil)
       return true, "Cleared client matcher list"
     elseif #split == 2 and split[1] == "get" and minetest.get_player_by_name(split[2]) then
-      minetest.chat_send_player(name, "[Name] protocol_version, ser_vers, major, minor, patch, version_string")
       local info = minetest.get_player_information(split[2])
-      local data = info.protocol_version.." ||| "..info.serialization_version.." ||| "..
-      info.major.." ||| "..info.minor.." ||| "..info.patch.." ||| "..info.version_string
-      minetest.chat_send_player(name, "["..split[2].."] "..data)
-    else return false, "Invalid Params: list/reset/get"
+      minetest.chat_send_player(name, get_string_by_info(info))
+    else return false, "Invalid Params: list/reset/get <name>"
     end
   end,
 })
